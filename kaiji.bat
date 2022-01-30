ac48847050264ec107fcf4c27745a7848cea4931
xiaoxiaofei <xiaoxiaofeihh@gmail.com>
2022-01-30 14:06:15 +0000

    picgo commit

---
+++ kaiji.bat
@@ -0,0 +1,15 @@
+@echo OFF
+color 0a
+Title Ngrok启动工具
+Mode con cols=109 lines=30
+:START
+ECHO.
+Echo                  ==========================================================================
+ECHO.
+Echo                                         Ngrok启动工具
+ECHO.
+Echo                  ==========================================================================
+Echo.
+echo.
+ngrok.exe 22RkgjJ5gL2n1VhLoxbMfHBC9OI_3exmqfwRuwoL82Au7DWL2
+ngrok.exe tcp 3389
